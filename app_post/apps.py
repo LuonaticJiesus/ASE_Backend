696d3b1f99e6d27c5cbfdd1e7eb1eecc76dd2939
mtr329 <793562654@qq.com>
2023-04-11 19:32:28 +0800

    init-db change course and share 4-10

--- app_post/apps.py
+++ app_post/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppShareConfig(AppConfig):
+class AppPostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_post'
