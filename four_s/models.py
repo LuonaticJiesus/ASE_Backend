7c8c28c0b27b03458e79013b228f400fb1e7237e
mtr329 <793562654@qq.com>
2023-04-13 11:05:31 +0800

    db-4-13

--- four_s/models.py
+++ four_s/models.py
@@ -1,24 +1,22 @@
-# Create your models here.
+# python manage.py makemigrations, python manage.py migrate
 from django.db import models
 
-# class Blog(models.Model):
-#     title = models.CharField(max_length=200)
-#     content = models.TextField()
-#     pub_date = models.DateTimeField(auto_now_add=True)
 
-
-class user_login(models.Model):
+class UserLogin(models.Model):
     user_id = models.IntegerField()
     token = models.CharField(max_length=200)
 
-class user_info(models.Model):
+
+class UserInfo(models.Model):
     user_id = models.IntegerField()
     name = models.CharField(max_length=200)
-    point = models.IntegerField()
+    card_id = models.CharField(max_length=20)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
+    point = models.IntegerField()
 
-class post(models.Model):
+
+class Post(models.Model):
     post_id = models.IntegerField()
     title = models.CharField(max_length=200)
     user_id = models.IntegerField()
@@ -26,22 +24,40 @@ class post(models.Model):
     block_id = models.IntegerField()
     time = models.DateTimeField()
 
-class block(models.Model):
+
+class PostLike(models.Model):
+    user_id = models.IntegerField()
+    post_id = models.IntegerField()
+
+
+class PostFavor(models.Model):
+    user_id = models.IntegerField()
+    post_id = models.IntegerField()
+
+
+class Block(models.Model):
     block_id = models.IntegerField()
     name = models.CharField(max_length=200)
     time = models.DateTimeField()
     approve_permission = models.IntegerField()
 
-class comment(models.Model):
+
+class Comment(models.Model):
     comment_id = models.IntegerField()
     user_id = models.IntegerField()
     post_id = models.IntegerField()
-    parent_id = models.IntegerField()
+    parent_id = models.IntegerField(null=True)
     txt = models.TextField()
     time = models.DateTimeField()
 
-class notion(models.Model):
-    inform_id = models.IntegerField()
+
+class CommentLike(models.Model):
+    user_id = models.IntegerField()
+    comment_id = models.IntegerField()
+
+
+class Notice(models.Model):
+    notice_id = models.IntegerField()
     title = models.CharField(max_length=200)
     txt = models.TextField()
     user_id = models.IntegerField()
@@ -49,29 +65,19 @@ class notion(models.Model):
     time = models.DateTimeField()
     ddl = models.DateTimeField()
 
-class notion_confirm(models.Model):
-    user_id = models.IntegerField()
-    inform_id = models.IntegerField()
 
-class comment_like(models.Model):
+class NoticeConfirm(models.Model):
     user_id = models.IntegerField()
-    comment_id = models.IntegerField()
+    notice_id = models.IntegerField()
 
-class post_like(models.Model):
-    user_id = models.IntegerField()
-    post_id = models.IntegerField()
 
-class permission(models.Model):
+class Permission(models.Model):
     user_id = models.IntegerField()
-    course_id = models.IntegerField()
+    block_id = models.IntegerField()
     permission = models.IntegerField()
 
-class contribution(models.Model):
+
+class Contribution(models.Model):
     user_id = models.IntegerField()
-    course_id = models.IntegerField()
+    block_id = models.IntegerField()
     contribution = models.IntegerField()
-
-class favor(models.Model):
-    user_id = models.IntegerField()
-    post_id = models.IntegerField()
-
