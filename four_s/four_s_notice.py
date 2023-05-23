b26c2b7cf3ae78c324b000b24c4059adac40291e
mtr329 <793562654@qq.com>
2023-05-23 21:43:06 +0800

    debug wrap_notice

--- four_s/four_s_notice.py
+++ four_s/four_s_notice.py
@@ -29,7 +29,7 @@ def wrap_notice(notice, user_id):
     n_dict['user_name'] = UserInfo.objects.get(user_id=notice.user_id).name
     n_dict['block_name'] = Block.objects.get(block_id=notice.block_id).name
     n_dict['confirm_state'] = 1 if NoticeConfirm.objects.filter(user_id=user_id).filter(
-        notice_id=notice).exists() else 0
+        notice_id=notice.notice_id).exists() else 0
     return n_dict
 
 
